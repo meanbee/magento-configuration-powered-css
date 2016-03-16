f0f0d655561b12d79f796cafb4f68f28cd81a907
Tom Robertshaw <me@tomrobertshaw.net>
2016-03-16 15:25:22 +0000

    Add high DPI media query helper function.

--- src/app/code/community/Meanbee/ConfigPoweredCss/Block/Css.php
+++ src/app/code/community/Meanbee/ConfigPoweredCss/Block/Css.php
@@ -3,7 +3,8 @@
 class Meanbee_ConfigPoweredCss_Block_Css extends Mage_Core_Block_Template
 {
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
 
         $this->setTemplate('meanbee/configpoweredcss/css.phtml');
@@ -18,5 +19,24 @@ class Meanbee_ConfigPoweredCss_Block_Css extends Mage_Core_Block_Template
     public function getConfig($path, $store = null)
     {
         return Mage::getStoreConfig($path, $store);
+    }
+
+    /**
+     * Get High DPI media query string
+     *
+     * @param float $ratio
+     * @return string
+     */
+    public function hidpi($ratio = 1.3)
+    {
+        $dpi = round($ratio * 96);
+        return <<<END
+@media only screen and (-webkit-min-device-pixel-ratio: $ratio),
+only screen and (min--moz-device-pixel-ratio: $ratio),
+only screen and (-o-min-device-pixel-ratio: $ratio/1),
+only screen and (min-resolution: {$dpi}dpi),
+only screen and (min-resolution: {$ratio}dppx)
+END;
+
     }
 }
